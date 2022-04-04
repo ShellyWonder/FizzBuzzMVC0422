1b259e51f07b2ab460bed3f7b493d1f90919433b
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-04-04 14:34:20 -0500

    Add project files.

---
+++ FizzBuzzMVC0422/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FizzBuzzMVC0422.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
