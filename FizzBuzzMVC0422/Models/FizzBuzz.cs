37a22c9e6acc4700a3d59a765c3dc12564356ccb
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-04-04 19:33:38 -0500

    created model

---
+++ FizzBuzzMVC0422/Models/FizzBuzz.cs
@@ -0,0 +1,11 @@
+﻿namespace FizzBuzzMVC0422.Models
+{
+    public class FizzBuzz
+    {
+        public int FizzValue { get; set; }
+        public int BuzzValue { get; set; }
+
+        public List<string> Result { get; set; } = new();
+
+    }
+}
